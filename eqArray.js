860f43060f2606be2c2cb25ce5f7801e755ab540
Diego Castro <diegojcr4@gmail.com>
2021-08-17 14:33:08 -0400

    Added code to compare array values

---
+++ eqArray.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed! ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed! ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+
+  return true;
+}
+
+// Testing
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
+assertEqual(eqArrays([], []), true); // => should PASS
+assertEqual(eqArrays(["1"], [1]), true); // => should FAIL
