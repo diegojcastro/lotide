1abada7997e2010348777abe1ab4bc4c42bfd069
Diego Castro <diegojcr4@gmail.com>
2021-08-23 13:08:41 -0400

    Refactored middleTest and tailTest to use M&C

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,22 @@
-const assertArraysEqual = require("../assertArraysEqual");
-const middle = require("../middle");
+const assert = require('chai').assert;
+const middle   = require('../middle');
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle(['hi', 'two']), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle(['one', 'two', 'three', 'four']), ['two', 'three']);
+describe("#middle", () => {
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([]), []);
+  });
+
+  it("returns [] for ['hi', 'two']", () => {
+    assert.deepEqual(middle(['hi', 'two']), []); 
+  });
+
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]); 
+  });
+
+  it("returns ['two', 'three'] for ['one', 'two', 'three', 'four']", () => {
+    assert.deepEqual(middle(['one', 'two', 'three', 'four']), ['two', 'three']); 
+  });
+
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,33 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 
-// General tests
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  it("returns length 2 for ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.equal(result.length, 2);
+  });
 
-// Testing original array unmodified
-console.log("Testing original array is unmodified:");
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  it("returns 'Lighthouse' for the first element of the tail of ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.equal(result[0], 'Lighthouse');
+  });
+
+  it("returns 'Labs' for the second element of the tail of ['Hello', 'Lighthouse', 'Labs']", () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.equal(result[1], 'Labs');
+  });
+
+  it("does not modify the original array from which .tail() is called", () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words); // no need to capture the return value since we are not checking it
+    assert.equal(words.length, 3); // original array should still have 3 elements!
+  });
+
+});
+
+
+// // Testing original array unmodified
+// console.log("Testing original array is unmodified:");
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
