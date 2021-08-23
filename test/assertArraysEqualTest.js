86aaf91b575fa9ff06e5e3b68a523faa588a5471
Diego Castro <diegojcr4@gmail.com>
2021-08-23 12:33:43 -0400

    Refactored assertArraysEqual testing and require eqArrays

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,5 @@
+const assertArraysEqual = require("../assertArraysEqual");
+
+assertArraysEqual([1,2,3], [1,2,3]);
+console.log("The following should fail:");
+assertArraysEqual(["typo", "nope"], ["typo", "nop"]);
